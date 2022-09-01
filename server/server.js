75984f6913ff50cc35c95f872fe60be7b13549c1
Bragren <bragren17@gmail.com>
2022-08-31 22:39:16 -0600

    updated server added mutations and queries

--- server/server.js
+++ server/server.js
@@ -16,18 +16,32 @@ const server = new ApolloServer({
   context: auth
 })
 
+const serverStart = async () => {
+  await server.start()
+  console.log('Started Server')
+}
+serverStart()
+
+server.applyMiddleware({app})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-server.applyMiddleware({app})
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API is running on port ${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
